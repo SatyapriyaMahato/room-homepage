5b362e8e9a7cb7d53d088d6044346a677716d1eb
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-05-04 18:11:46 +0530

    Add slide image and bug fix

--- js/script.js
+++ js/script.js
@@ -15,3 +15,50 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+var slideIndex = 1;
+var images = ["images/desktop-image-hero-1.jpg", "images/desktop-image-hero-2.jpg", "images/desktop-image-hero-3.jpg"];
+
+var headings = ["Discover innovative ways to decorate", " We are available all across the globe", "Manufactured with the best materials"];
+  
+var descriptions = [
+
+"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love." ,
+
+"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office." ,
+
+"With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.", 
+ 
+];
+  
+  
+showSlide(slideIndex);
+
+function nextSlide() {
+    showSlide(slideIndex += 1);
+}
+
+function prevSlide() {
+    showSlide(slideIndex -= 1);
+}
+
+function showSlide(n) {
+    var slides = document.getElementsByClassName("hero-img");
+    console.log(slides);
+    var heroHead = document.getElementById("hero-heading");
+    var heroText = document.getElementById("hero-description");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].setAttribute("src", images[slideIndex - 1]);
+    heroHead.innerHTML = headings[slideIndex - 1];
+    heroText.innerHTML = descriptions[slideIndex - 1];
+}
