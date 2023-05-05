74e05821d1f44aa0d69d5209d2206f7fff08651a
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-05-05 17:57:32 +0530

    Improve script,js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+navLink.forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
 
 
-var slideIndex = 1;
-var images = ["images/desktop-image-hero-1.jpg", "images/desktop-image-hero-2.jpg", "images/desktop-image-hero-3.jpg"];
-
-var headings = ["Discover innovative ways to decorate", " We are available all across the globe", "Manufactured with the best materials"];
-  
-var descriptions = [
+let slideIndex = 1;
+let headings = ["Discover innovative ways to decorate", " We are available all across the globe", "Manufactured with the best materials"]; 
+let descriptions = [
 
 "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love." ,
 
@@ -32,33 +26,31 @@ var descriptions = [
  
 ];
   
-  
+
+const showSlide = n => {
+  const slides = document.getElementsByClassName("hero-img");
+  const heroHead = document.getElementById("hero-heading");
+  const heroText = document.getElementById("hero-description");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+  heroHead.innerHTML = headings[slideIndex - 1];
+  heroText.innerHTML = descriptions[slideIndex - 1];
+};
+
+const nextSlide = () => {
+  showSlide(slideIndex += 1);
+};
+
+const prevSlide = () => {
+  showSlide(slideIndex -= 1);
+};
+
 showSlide(slideIndex);
-
-function nextSlide() {
-    showSlide(slideIndex += 1);
-}
-
-function prevSlide() {
-    showSlide(slideIndex -= 1);
-}
-
-function showSlide(n) {
-    var slides = document.getElementsByClassName("hero-img");
-    console.log(slides);
-    var heroHead = document.getElementById("hero-heading");
-    var heroText = document.getElementById("hero-description");
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-      slideIndex = slides.length
-    }
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-    slides[slideIndex - 1].setAttribute("src", images[slideIndex - 1]);
-    heroHead.innerHTML = headings[slideIndex - 1];
-    heroText.innerHTML = descriptions[slideIndex - 1];
-}
